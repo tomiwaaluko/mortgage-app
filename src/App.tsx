5072fae2f90773e897d42eb0faf63d7189cc59f1
Luis Alban <alalban@hotmail.com>
2025-06-28 10:50:43 -0700

    EmploymentInfoForm fully functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,45 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { WelcomePage } from "./components/WelcomePage";
 import { PersonalInfoPage } from "./components/PersonalInfoPage";
+import { EmploymentInfoPage } from "./components/EmploymentInfoPage";
 import { Layout } from "./components/Layout";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <WelcomePage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/personal-info"
-          element={
-            <Layout>
-              <PersonalInfoPage />
-            </Layout>
-          }
-        />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <WelcomePage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/personal-info"
+            element={
+              <Layout>
+                <PersonalInfoPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/employment-info"
+            element={
+              <Layout>
+                <EmploymentInfoPage />
+              </Layout>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
